4134ad09009e7166f8ed81ae3fd2b1dfb1df5907
RemiThorez <remi.thorez@gmail.com>
2024-10-23 18:08:14 -0400

    Sa fonctionne... Presque

--- tp1-gestion-vehicule/src/gestionVehicule/GestionVehicule.jsx
+++ tp1-gestion-vehicule/src/gestionVehicule/GestionVehicule.jsx
@@ -1,6 +1,7 @@
 import React, {Component, useState} from 'react'
 import Vehicule from './Vehicule';
 import ManufactureVehicule from './ManufactureVehicule';
+import axios from 'axios';
 
 
 class GestionVehicule extends Component
@@ -15,6 +16,8 @@ class GestionVehicule extends Component
             ajouterVehiculeActif: false,
             ajouterVehiculeVINActif: false,
             ajouterVehiculeComplexeActif: false,
+            optionsModeles: [],
+            optionsFabricants: []
         }
 
         this.manufactureVehicule = new ManufactureVehicule();
@@ -71,7 +74,7 @@ class GestionVehicule extends Component
         this.setState((state => ({vehicules:[...state.vehicules, vehicule], ajouterVehiculeActif: false})));
     }
     //"JS2YB5A20A6300765"
-     creerVehiculeVIN = async (e) =>
+    creerVehiculeVIN = async (e) =>
     {
         e.preventDefault();
         const donneeFormulaire = new FormData(e.target);
@@ -82,6 +85,47 @@ class GestionVehicule extends Component
         if(vehicule)
         {
             this.setState((state => ({vehicules:[...state.vehicules, vehicule], ajouterVehiculeVINActif: false})));
+        }
+    }
+    
+    creerVehiculeComplexe = async (e) =>
+    {
+        e.preventDefault();
+
+        const donneeFormulaire = new FormData(e.target);
+
+        const fabricant = donneeFormulaire.get("fabricant");
+        const modele = donneeFormulaire.get("modele");
+
+        if(fabricant != "" && modele != "" && modele != 0)
+        {
+            //Ajout du véhicule
+            const vehicule = this.manufactureVehicule.CreerVehicule(fabricant,modele,donneeFormulaire.get("annee"),++this.state.vehicules.length)
+        
+            this.setState((state => ({vehicules:[...state.vehicules, vehicule], ajouterVehiculeComplexeActif: false})));
+        }
+        else if(fabricant == "") 
+        {
+            // On cherche le fabricant
+            if(modele == "")
+            {
+                alert("Le modèle et le fabricant sont vides !\n Merci d'entrer au moins une de c'est deux informations.");
+            }
+            else
+            {
+                const reponse = await axios.get()
+            }
+        }
+        else
+        {
+            // On cherche le modèle
+            const reponse = await axios.get(`https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMakeYear/make/${fabricant}/modelyear/${donneeFormulaire.get("annee")}?format=json`)
+            let optionsModele = reponse.data.Results.map((modele)=> <option value={modele.Model_Name}>{modele.Model_Name}</option>);
+            if(optionsModele.length == 0)
+            {
+                optionsModele.push(<option value={0}>Aucun modèle trouvé pour cette date et ce fabricant.</option>)
+            }
+            this.setState({optionsModeles: optionsModele});
         }
     }
 
@@ -96,10 +140,12 @@ class GestionVehicule extends Component
 
         return(
             <>
+            {!this.state.cacherVehicule &&
+            <div>
                 <div>
                     <button onClick={this.gererBtnAjouterVehicule}>Ajouter véhicule</button>
                     <button onClick={this.gererBtnAjouterVehiculeVIN}>Ajouter véhicule avec VIN</button>
-                    <button onClick={this.gererBtnAjouterVehiculeComplexe}>Ajouter véhicule complexe</button>
+                    <button onClick={this.gererBtnAjouterVehiculeComplexe}>Ajouter véhicule avec informations manquantes</button>
                 </div>
            
                 <div> 
@@ -132,13 +178,42 @@ class GestionVehicule extends Component
 
                 <div>
                     {this.state.ajouterVehiculeComplexeActif &&
-                    <form onSubmit={""}>
+                    <form onSubmit={this.creerVehiculeComplexe}>
                         <h2>Ajouter véhicule avec informations manquantes</h2>
+                        <label>Fabricant: </label>
+                        {this.state.optionsFabricants.length == 0 ?
+                        (
+                            <input type="text" name='fabricant'></input>
+                        ):
+                        (
+                            <select name="fabricant">
+                                {this.state.optionsFabricants}
+                            </select> 
+                        )
+                        }
+                        
+                        <label>Modèle: </label>
+                        {this.state.optionsModeles.length == 0 ?
+                        (
+                            <input type="text" name='modele'></input>
+                        ):
+                        (
+                            <select name="modele">
+                                {this.state.optionsModeles}
+                            </select> 
+                        )
+                        }
+                        <label>Année de fabrication: </label>
+                        <select name="annee" required>
+                                    {optionsAnnee}
+                        </select>
                         <button type="submit">Ajouter</button>
                         <button onClick={this.gererBtnAnnulerAjoutVehiculeComplexe}>Annuler</button>
                     </form>}
                 </div>
+                </div>}
                 {this.state.vehicules.map((vehicule) => vehicule)}
+                
             </>
         );
     }
